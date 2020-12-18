3c0c911e0946af734af35a655afd0531b37982fd
Mikhaelyes <74400761+Mikhaelyes@users.noreply.github.com>
2020-12-18 03:23:15 +0300

    Update DIRsem.c

--- DIRsem.c
+++ DIRsem.c
@@ -6,11 +6,14 @@
 #include <sys/types.h>
 #include <string.h>
 #include <dirent.h>
+#include <assert.h>
 
-main(int argc, char** argv, char** envp)
+int main(int argc, char** argv, char** envp)
 {
+	assert (argc > 1);
 	char* dir = argv[1];
 	DIR* dh = opendir(dir);
+	assert(dir != NULL);
 	struct dirent* file;
 	while ((file = readdir(dh))) {
 		printf("%s\n", file->d_name);
